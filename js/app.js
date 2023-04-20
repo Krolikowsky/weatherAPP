d54be44adee4557663acfd48e49ba460a926ae35
Szymon Królikowski <kontakt@szymonkrolikowski.pl>
2023-04-20 23:54:14 +0200

    ADD connection with API

--- js/app.js
+++ js/app.js
@@ -22,3 +22,38 @@ const weatherForecast = document.querySelector(".weatherForecast__description")
 const sunriseParagraph = document.querySelector(".moreInformation__sunrise")
 const sunsetParagraph = document.querySelector(".moreInformation__sunset")
 
+searchIcon.addEventListener("click", () => {
+    searchCityName = searchInput.value
+    connectAPI()
+})
+
+window.addEventListener('DOMContentLoaded', () => connectAPI())
+
+const connectAPI = () => {
+    const API_KEY = `d531ef0a02c5a713565a1b96de1fa8df`
+    const API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCityName}&appid=${API_KEY}&units=metric`
+
+    hourlySlider.innerHTML = ""
+    dailySlider.innerHTML = ""
+
+    fetch(API_URL)
+        .then(response => response.json())
+        .then((response) => {
+            downloadCityName(response)
+            downloadMainInformation(response)
+            downloadHourlyWeather(response)
+            downloadDailyWeather(response)
+            downloadSunrise(response)
+            downloadSunset(response)
+            errorInformation.classList.remove("displayBlock")
+            searchInput.style.borderBottom = "1px solid #D8D8D8"
+            console.log(response)
+        })
+        .catch((error) => {
+            errorInformation.classList.add("displayBlock")
+            searchInput.style.borderBottom = "1px solid #FF0000"
+            errorInformation.textContent = `The city name is incorrect: ${searchCityName}`
+        })
+
+    timeUpdate()
+}
